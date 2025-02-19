89bf229809b061a7293ca764f4a4194db2177dbc
wangying <wangying2@liepin.com>
2025-02-19 16:48:32 +0800

    添加了一个coneole

---
+++ git/test1.js
@@ -0,0 +1,10 @@
+console.log('测试fetch222');
+
+function test (fn) {   
+  return fn(...arg)
+}
+console.log('这又是一个测试啊');
+console.log('111111');
+console.log('222222');
+console.log('333333');
+console.log('后面的console')
